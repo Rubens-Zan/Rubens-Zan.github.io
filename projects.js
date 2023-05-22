d8e8823b1d08807ea67056585241a292093d26a7
Rubens Zandomenighi Laszlo <80857600+Rubens-Zan@users.noreply.github.com>
2023-05-22 14:25:09 -0300

    Update projects.js

--- projects.js
+++ projects.js
@@ -4,34 +4,39 @@ export default [
         "description": "Esse projeto foi feito para a disciplina de Programação 2, a partir da biblioteca para implementação de jogos gráficos Allegro 5, com a linguagem C.",
         "img": "boulder-dash.png",
         "link": "https://github.com/Rubens-Zan/Boulder-Dash-C",
-        "languages": "C"
+        "languages": "C",
+        "staticPath" : true
     },
     {
         "title": "Landing Page Divulgação de livros",
         "description": "Projeto pessoal de divulgação de livros do autor Tonny Robbins. ",
         "img": "Tony-robbinson.png",
         "link": "https://github.com/Rubens-Zan/Divulgacao-livros",
-        "languages": "HTML,CSS,JAVASCRIPT."
+        "languages": "HTML,CSS,JAVASCRIPT.",
+        "staticPath" : true
     },
     {
         "title": "Controlador de Transações",
         "description": "Desenvolvimento de um controlador de concorrência em transações, utilizando os métodos de Equivalência por visão e seriação de conflito",
         "img": "https://user-images.githubusercontent.com/80857600/239979442-6f355d27-afb7-47bd-8a87-9469bab39897.png",
         "link": "https://github.com/Rubens-Zan/Transaction-escalation",
-        "languages": "C"
+        "languages": "C",
+        "staticPath" : false
     },
     {
         "title": "Mensageiro Local",
         "description": "Implementação de sistema para o envio de mensagens (textuais e arquivos de qualquer formato de mídia) utilizando o protocolo TCP/IP, utilizando computadores distintos ligados por um cabo ethernet.",
         "img": "https://user-images.githubusercontent.com/80857600/239978169-378749c4-3fba-4ab2-a14b-73c82e28f1e4.png",
         "link": "https://github.com/Rubens-Zan/Mensageiro-Local",
-        "languages": "C"
+        "languages": "C",
+        "staticPath" : false
     },
     // {
     // 	"title": "",
     // 	"description":"",
     // 	"img":"",
     // 	"link": "",
-    // 	"languages": ""
+    // 	"languages": "",
+    //   "staticPath" : true
     // },
 ]
